0f02d07a90a79f55879405916600d39aa4c9692c
DevanshuRastogi <125262981+DevanshuRastogi@users.noreply.github.com>
2025-01-25 20:08:55 +0530

    done day-1

---
+++ Day-2/react-tours-project/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import NavBar from './components/NavBar'
+import Hero from './components/Hero'
+import About from './components/About'
+import Services from './components/Services'
+import Tours from './components/Tours'
+import Footer from './components/Footer'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+ <>
+    <NavBar />
+    <Hero />
+    <About />
+    <Services />
+    <Tours  />
+    <Footer  />
+ </>
+  )
+}
+
+export default App
---
+++ Day-2/react-tours-project/src/components/NavBar.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import logo from "../images/logo.svg";
+import { navElements } from "../data";
+
+const NavBar = () => {
+  return (
+    <nav className="navbar">
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} className="nav-logo" alt="backroads" />
+          <button type="button" className="nav-toggle" id="nav-toggle">
+            <i className="fas fa-bars"></i>
+          </button>
+        </div>
+
+        <ul className="nav-links" id="nav-links">
+          {navElements.map((nav) => {
+            const { id, href, navTitle } = nav;
+           return  <li key={id}>
+           <a href={href} className="nav-link">
+             {navTitle}{" "}
+           </a>
+         </li>;
+          })}
+        </ul>
+
+        <ul className="nav-icons">
+          <li>
+            <a
+              href="https://www.twitter.com"
+              target="_blank"
+              className="nav-icon"
+            >
+              <i className="fab fa-facebook"></i>
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.twitter.com"
+              target="_blank"
+              className="nav-icon"
+            >
+              <i className="fab fa-twitter"></i>
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.twitter.com"
+              target="_blank"
+              className="nav-icon"
+            >
+              <i className="fab fa-squarespace"></i>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
