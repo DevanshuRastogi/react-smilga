c67d9c45c15abb4bde4483d1891db74db7e42e36
DevanshuRastogi <125262981+DevanshuRastogi@users.noreply.github.com>
2025-02-01 11:13:37 +0530

    conditional rendereing

--- Day-3/advanced-react/src/App.jsx
+++ Day-3/advanced-react/src/App.jsx
@@ -1,9 +1,29 @@
-
-import Starter from './tutorial/02-useEffect/starter/02-useEffect-basics'
+import { useState } from "react";
+import Starter from "./tutorial/03-conditional-rendering/starter/07-user-challenge";
 function App() {
+  const [users, setUsers] = useState(null);
+
+  const login = () => {
+    setUsers("John");
+  };
+  const logout = () => {
+    setUsers(null);
+  };
+
   return (
-    <div className='container'>
-      <Starter />
+    <div className="container">
+      {users ? (
+        <>
+          <h4>hello there {users}</h4>
+          <button className="btn" onClick={logout}> logout</button>
+        </>
+      ) : (
+        <>
+          {" "}
+          <h4>Please Login</h4>
+          <button className="btn" onClick={login}> login</button>
+        </>
+      )}
     </div>
   );
 }
