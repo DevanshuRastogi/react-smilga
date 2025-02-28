5a1c40e2ed159d4d3c46a2a0100f07b4f2404e18
DevanshuRastogi <125262981+DevanshuRastogi@users.noreply.github.com>
2025-02-28 20:26:55 +0530

    day 5 2 examples done

--- Day-5/src/examples/1-first-request.js
+++ Day-5/src/examples/1-first-request.js
@@ -1,10 +1,18 @@
+import axios from 'axios';
 import { useEffect } from 'react';
 // limit, if 429 wait for 15 min and try again
 const url = 'https://www.course-api.com/react-store-products';
 
+
+const fetchReq= async()=>{
+
+  const respo = await axios(url);
+  const data = await respo.data;
+  console.log(data)
+}
 const FirstRequest = () => {
   useEffect(() => {
-    console.log('first axios request');
+    fetchReq();
   }, []);
 
   return <h2 className='text-center'>first request</h2>;
