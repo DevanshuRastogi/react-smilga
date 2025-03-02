70af40e8aa35f4f5ed882261cec6fe5d13ce3a42
DevanshuRastogi <125262981+DevanshuRastogi@users.noreply.github.com>
2025-03-02 11:28:00 +0530

    startng mixmaster project react router + react query commit  2

--- Day-7/src/main.jsx
+++ Day-7/src/main.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import './index.css';
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const router = createBrowserRouter(
+ [ {
+    path: "/",
+    element: <h1>Home page</h1>,
+  },
+
+  {
+    path: "/about",
+    element: (
+      <div>
+        <h1>About Page</h1>
+      </div>
+    ),
+  }]
 );
+const App = ()=>{
+  return <RouterProvider router={router} />
+  
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
+
+export default  App;
